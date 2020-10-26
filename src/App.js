5b794c16082084c6c03f31b5f7d69b5c51326c88
Rohit Dubey <dubey22rohit@gmail.com>
2020-10-26 13:23:30 +0530

    Project Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import './App.css';
+import React from "react";
+import Card from "./components/Card";
+import InfiniteScroll from "react-infinite-scroll-component";
+import colleges from "./assets/data/colleges.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: colleges,
+      items: Array.from({ length: 10 }),
+      hasMore: true,
+    };
+  }
+
+  fetchMoreData = () => {
+    //stop api call when all the data is popped
+    if (this.state.items.length >= this.state.data.colleges.length) {
+      this.setState({ hasMore: false });
+      return;
+    }
+
+    // a fake async api call like which sends
+    // 10 more records in 1.5 secs
+    setTimeout(() => {
+      this.setState({
+        items: this.state.items.concat(Array.from({ length: 10 })),
+      });
+    }, 1500);
+  };
+
+  render() {
+    const { data, items, hasMore } = this.state;
+    return (
+      <div className="App">
+        <p className="app_title">Colleges in North India</p>
+
+        <InfiniteScroll
+          dataLength={items.length}
+          next={this.fetchMoreData}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>That's All Folks! You have seen it all</b>
+            </p>
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {items.map((i, index) => (
+            <>
+              <Card key={index} data={data.colleges[index]}></Card>
+            </>
+          ))}
+        </InfiniteScroll>
+      </div>
+    );
+  }
 }
 
 export default App;
